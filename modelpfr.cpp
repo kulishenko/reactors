d77b832a2dbcb50b7e3c50a0571fece865d93d4a
Roman Kulishenko <admin@sa.lti-gti.ru>
2016-01-21 03:49:19 +0300

    *new files were added

---
+++ modelpfr.cpp
@@ -0,0 +1,6 @@
+#include "modelpfr.h"
+
+ModelPFR::ModelPFR()
+{
+
+}
---
+++ modelpfr.h
@@ -0,0 +1,11 @@
+#ifndef MODELPFR_H
+#define MODELPFR_H
+
+
+class ModelPFR : public ModelItem
+{
+public:
+    ModelPFR();
+};
+
+#endif // MODELPFR_H
